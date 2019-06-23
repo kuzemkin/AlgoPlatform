db65fcb3d5e426130f67bf7e79c282f747683ef6
Denis Kuziomkin <dkuzemkin@yandex.ru>
2019-06-23 21:25:08 +0300

    FIX_23.06.2019

--- ATP/Form1.cs
+++ ATP/Form1.cs
@@ -40,8 +40,8 @@ namespace ATP
         /// <param name="e"></param>
         private void LostFocus1_Method(object sender, EventArgs e)
         {
-            Login = textBox1.Text;
-            
+            //Login = textBox1.Text;
+            Login = "kuziomkin";
         }
         /// <summary>
         /// Метод инициализации поля Password
@@ -50,8 +50,8 @@ namespace ATP
         /// <param name="e"></param>
         private void LostFocus2_Method(object sender, EventArgs e)
         {
-            Password = textBox2.Text;
-           
+            //Password = textBox2.Text;
+            Password = "QJHAWU";
         }
         /// <summary>
         /// Метод подключения к серверу
