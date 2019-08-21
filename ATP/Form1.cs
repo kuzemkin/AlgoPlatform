e15ce36c08de04e0d8f9ace111dc41c216de296e
Denis Kuziomkin <dkuzemkin@yandex.ru>
2019-08-21 22:25:14 +0300

    FIX 21082019 22:25

--- ATP/Form1.cs
+++ ATP/Form1.cs
@@ -32,9 +32,9 @@ namespace ATP
         public List<Collections.Tick> TicksList = new List<Collections.Tick>();
         public List<double> SDeviation = new List<double>();
         public int n = 100;                  //количество запрашиваемых баров 
-        public static int nBars = 100;        //количество баров для отрезка экстремумов
+        public static int nBars = 50;        //количество баров для отрезка экстремумов
         public int ind = nBars;              //начальный индекс  
-        public int sma=200;                //количество баров для скользящей средней  
+        public int sma=100;                //количество баров для скользящей средней  
         public double money;
         /// <summary>
         /// Инициалезация компонентов
@@ -449,7 +449,7 @@ namespace ATP
                         SDeviation.Add(b.GetRange(l, nBars).Select(p => p.Close).Max() - b.GetRange(l, nBars).Select(p => p.Close).Min());
                         if (i > sma)
                         {
-                            if (b[i].Close> b.GetRange(l, nBars).Select(p => p.High).Max() & b[i].Close> SMA(b.GetRange(i - sma, sma), sma) & SDeviation.Last()>(SDeviation.GetRange(SDeviation.Count()-nBars,nBars).Average() + 3*SDeviationCalculate(SDeviation.GetRange(SDeviation.Count() - nBars, nBars))))
+                            if (b[i].Close> b.GetRange(l, nBars).Select(p => p.High).Max() & b[i].Close> SMA(b.GetRange(i - sma, sma), sma) & SDeviation.Last()>(SDeviation.GetRange(SDeviation.Count()-nBars,nBars).Average() + SDeviationCalculate(SDeviation.GetRange(SDeviation.Count() - nBars, nBars))))
                             {
                                 BuyOrder(b, t, i);             
                             }
