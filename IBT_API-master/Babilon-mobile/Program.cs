c2adafb5ff0fb6acfbd591272937f1a5f9966a2a
Denis Kuziomkin <dkuzemkin@yandex.ru>
2020-01-21 19:20:08 +0300

    FIX 21012020

--- IBT_API-master/Babilon-mobile/Program.cs
+++ IBT_API-master/Babilon-mobile/Program.cs
@@ -7,10 +7,10 @@ namespace Babilon_mobile
     {
         static void Main(string[] args)
         {
-            //Requests.GetRate("901117077").RespInfo();
+            Requests.GetRate("5058270374080470").RespInfo();
             //Requests.GetSMS("918630100", "5058270221000580").ResponseInfo();
-            Requests.P2P("5058270221000580", "0322", "334", 100, "443", DateTime.Now.Ticks.ToString(), "C2C", "5058270222006644", "9802CEBC-67AA-41EA-8F1F-50DF8B31A099").ResponseInfo();
-            //Requests.Payment("20218810000001022811",DateTime.Now.Ticks,100,99,1, "2021881", "9802CEBC-67AA-41EA-8F1F-50DF8B31A099").ResponseInfo();            
+            //Requests.P2P("5058270221000580", "0322", "334", 100, "443", DateTime.Now.Ticks.ToString(), "C2C", "5058270222006644", "9802CEBC-67AA-41EA-8F1F-50DF8B31A099").ResponseInfo();
+            //Requests.Payment("901117077", DateTime.Now.Ticks,100,99,1, "2021881", "9802CEBC-67AA-41EA-8F1F-50DF8B31A099").ResponseInfo();            
             Console.ReadKey();            
         }
     }
